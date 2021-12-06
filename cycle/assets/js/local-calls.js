d96361ca0441f83d0a62304ad0ced3d4115ac102
orossh <owenhaynes86@gmail.com>
2021-12-06 20:15:47 +0000

    Added slideshow

---
+++ cycle/assets/js/local-calls.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+$('.gallery').cycle({
+		fx:    'zoom', 
+        sync:  false, 
+        delay: -5000 // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+
+});
